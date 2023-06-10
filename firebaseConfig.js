9146a9cc3861ef9f5c77757ae84efc321eab47c9
Razin Syakib <muhammadrazin14@gmail.com>
2023-06-10 18:03:27 +0700

    can get id token with sign in with google method

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBbkK9FEIROolY29Z2FTrEAJpWaqe5TxzI",
+  authDomain: "silent-circlet-381002.firebaseapp.com",
+  projectId: "silent-circlet-381002",
+  storageBucket: "silent-circlet-381002.appspot.com",
+  messagingSenderId: "840215620988",
+  appId: "1:840215620988:web:24f782eac3c96a7ff848f9",
+  measurementId: "G-1XPCT4P56F"
+};
